921c6838492944db447f82fe5128fa59bd68e5c7
Rocio Arellano Lopez <rocioarellano_94@hotmail.com>
2022-06-14 20:31:32 +0200

    webinar

--- src/main/java/com/ironhack/homework_2/CRM/Webinar.java
+++ src/main/java/com/ironhack/homework_2/CRM/Webinar.java
@@ -2,15 +2,36 @@ package com.ironhack.homework_2.CRM;
 
 import com.ironhack.homework_2.Classes.Lead;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Webinar {
 
     private String name;
     private long phoneNumber;
     private String email;
     private String companyName;
+    private List<String> leadData;
 
-    public Lead signingUp(String name, long phoneNumber, String email, String companyName) {
-        Lead lead = new Lead(name, phoneNumber, email, companyName);
+
+
+
+    public Lead signingUp() {
+        leadData = getInputData("Introduce your name: \n","Introduce your phone number \n", "Introduce your email \n", "Introduce your company name\n");
+        Lead lead = new Lead(leadData.get(0),Long.parseLong(leadData.get(1)) , leadData.get(2), leadData.get(3));
         return lead;
+    }
+
+    private static List<String> getInputData(String... questions) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> inputData = new ArrayList<>();
+        for (String question : questions) {
+            System.out.println(question);
+            inputData.add(scanner.next());
+        }
+
+        return inputData;
     }
 }
--- src/main/java/com/ironhack/homework_2/Main/Main.java
+++ src/main/java/com/ironhack/homework_2/Main/Main.java
@@ -1,6 +1,7 @@
 package com.ironhack.homework_2.Main;
 
 import com.ironhack.homework_2.CRM.CRM;
+import com.ironhack.homework_2.CRM.Webinar;
 import com.ironhack.homework_2.Classes.Account;
 import com.ironhack.homework_2.Classes.Contact;
 import com.ironhack.homework_2.Classes.Lead;
@@ -8,11 +9,16 @@ import com.ironhack.homework_2.Classes.Opportunity;
 import com.ironhack.homework_2.Enumerations.IndustryEnum;
 import com.ironhack.homework_2.Enumerations.ProductEnum;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Webinar webinar = new Webinar();
+
+
         //Create a Scanner to collect user input
 
         Scanner myScanner = new Scanner(System.in);
@@ -82,12 +88,21 @@ public class Main {
                                 case 2:
                                     industryEnum = IndustryEnum.ECOMMERCE;
                                     break;
+                                case 3:
+                                    industryEnum = IndustryEnum.MANUFACTURING;
+                                    break;
+                                case 4:
+                                    industryEnum = IndustryEnum.MEDICAL;
+                                    break;
+                                case 5:
+                                    industryEnum = IndustryEnum.OTHER;
+                                    break;
 
                             }
-                            System.out.println(" ");
-                            //int productQuantity = myScanner.nextInt();
 
-                            Account account = crm.createAccount();
+                            List<String> companyData = getInputData("How many employees does your company have?\n","Introduce company's city\n", "Introduce company's country\n");
+
+                            Account account = crm.createAccount(industryEnum, Integer.parseInt(companyData.get(0)), companyData.get(1), companyData.get(2), contact, opportunity);
                             System.out.println("convert done");
                             break;
                         case "call":
@@ -123,4 +138,18 @@ public class Main {
         }
         return false;
     }
+
+
+
+    private static List<String> getInputData(String... questions) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> inputData = new ArrayList<>();
+        for (String question : questions) {
+            System.out.println(question);
+            inputData.add(scanner.next());
+        }
+
+        return inputData;
+    }
+
 }
